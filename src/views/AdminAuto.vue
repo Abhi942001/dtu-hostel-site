52deb3dc2daa96756a821e36c3aa46e688ee9387
Abhishek <abhishek942001@gmail.com>
2021-05-06 17:07:50 +0530

    Improved Navigation and color

--- src/views/AdminAuto.vue
+++ src/views/AdminAuto.vue
@@ -1,43 +1,39 @@
 <template>
-	<v-container style="width:100%">
-		<HelloWorld style="margin-bottom: 20px;" />
-		<v-card style="padding: 20px;">
-			<div class="text-h3">
-				<b>Auto Allotment</b>
-			</div>
-		</v-card>
-		<v-divider style="margin:20px 0px 20px" />
-		<v-card
-			class="d-flex justify-center table-container"
-			style="width:100%"
-		>
-			<StudentSelectTable />
-		</v-card>
-	</v-container>
+  <v-container style="width: 100%">
+    <v-card style="padding: 20px">
+      <div class="text-h3">
+        <b>Auto Allotment</b>
+      </div>
+    </v-card>
+    <v-divider style="margin: 20px 0px 20px" />
+    <v-card class="d-flex justify-center table-container" style="width: 100%">
+      <StudentSelectTable />
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 import StudentSelectTable from "@/components/StudentSelectTable";
-import HelloWorld from "@/components/HelloWorld";
-export default {
-	components: { StudentSelectTable, HelloWorld },
-	name: "AdminAuto",
-	data: () => ({}),
 
-	beforeRouteEnter(to, from, next) {
-		const user = JSON.parse(localStorage.getItem("user"));
-		if (user ? user.utype !== "admin" : true) {
-			next("/login");
-		} else {
-			next();
-		}
-	},
+export default {
+  components: { StudentSelectTable },
+  name: "AdminAuto",
+  data: () => ({}),
+
+  beforeRouteEnter(to, from, next) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user ? user.utype !== "admin" : true) {
+      next("/login");
+    } else {
+      next();
+    }
+  },
 };
 </script>
 
 <style>
 .table-container {
-	width: 80%;
-	padding: 20px;
+  width: 80%;
+  padding: 20px;
 }
 </style>
